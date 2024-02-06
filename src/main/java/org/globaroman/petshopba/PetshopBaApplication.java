b0551d99b7fb581d45a96dcb965c324054614d98
Roman Globa <globaroman@gmail.com>
2024-02-07 00:06:39 +0200

    Created project

---
+++ src/main/java/org/globaroman/petshopba/PetshopBaApplication.java
@@ -0,0 +1,13 @@
+package org.globaroman.petshopba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopBaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetshopBaApplication.class, args);
+    }
+
+}
